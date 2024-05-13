e2c635fd6e67fe76e5ac20252e39b3a26c424c4d
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-13 14:45:06 +0000

    set up index view and navbar template

--- perfect_fit/settings.py
+++ perfect_fit/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount'
+    'allauth.socialaccount',
+
+    # My Apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +65,10 @@ ROOT_URLCONF = 'perfect_fit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
