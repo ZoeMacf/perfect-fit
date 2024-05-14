c3e1aa3b7891161f414aa65e5d128094f4b1e257
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-14 22:25:38 +0000

    add puzzle_exchange model

--- puzzle_exchange/models.py
+++ puzzle_exchange/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import UserProfile
 
-# Create your models here.
+class PuzzleExchange(models.Model):
+
+ submission_title = models.CharField(max_length=200, unique=True)
+ poster = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='puzzle_submission')
+ submitted_on = models.DateTimeField(auto_now=True)
+ approved_submission = models.BooleanField(default=False)
+ submission_text = models.TextField()
+ puzzle_image = models.CloudinaryField('image', default='placeholder')
+
+ def __str__(self):
+          return f"{self.submission_title}"
