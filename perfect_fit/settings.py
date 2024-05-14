98b6606bf847058e2a35ac428fd3f942e559f699
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-14 22:17:14 +0000

    add models for checkout, users and newsletter

--- perfect_fit/settings.py
+++ perfect_fit/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'home',
     'bag',
     'checkout',
+    'newsletter',
     'products',
     'puzzle_exchange',
     'users'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django_countries.fields import CountryField
 
-# Create your models here.
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  display_name = models.CharField(max_length=20, null=True, blank=True)
+  user_image = CloudinaryField("image", default="placeholder")
+  user_phone_number = models.CharField(max_length=20, null=True, blank=True)
+  user_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+  user_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+  user_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+  user_county = models.CharField(max_length=80, null=True, blank=True)
+  user_postcode = models.CharField(max_length=20, null=True, blank=True)
+  user_country = CountryField(blank_label='Country', null=True, blank=True)
+
+  def __str__(self):
+    return self.UserProfile.display_name
