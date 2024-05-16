12b7fe649408ea857d6734c0dffaafe9a8487b0c
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-16 21:02:29 +0000

    add puzzle_photo field to user_msgs model

--- user_msgs/models.py
+++ user_msgs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
 from users.models import UserProfile
 
 class UserMessage(models.Model):
      sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sender')
      reciever = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reciever')
      msg_content = models.TextField()
+     puzzle_photo = CloudinaryField('image', default='placeholder')
      created_at = models.DateTimeField(auto_now=True)
