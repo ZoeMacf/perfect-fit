db15a711c8bdeb661acd515461b637104f00b43f
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-17 12:19:41 +0000

    set up all products view and template

--- perfect_fit/settings.py
+++ perfect_fit/settings.py
@@ -29,9 +29,9 @@ if os.path.exists("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-zoemacf-perfectfit-myjy0wrtjrs.ws-eu114.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-zoemacf-perfectfit-myjy0wrtjrs.ws-eu111.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
