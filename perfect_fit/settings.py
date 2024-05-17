2ab1ed9b173f10b848dc5f63f4890dbb0a77be93
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-17 17:16:25 +0000

    add border in between products

--- perfect_fit/settings.py
+++ perfect_fit/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-zoemacf-perfectfit-myjy0wrtjrs.ws-eu111.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-zoemacf-perfectfit-myjy0wrtjrs.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
