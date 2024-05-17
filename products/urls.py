b9f3ddd50234fa19f256ae6ba531a0d924d61396
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-17 17:32:16 +0000

    create product_detail view and template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-  path('', views.all_products, name='all_products')
+  path('', views.all_products, name='all_products'),
+  path('<int:product_id>/', views.product_detail, name='product_detail'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -8,10 +8,21 @@ def all_products(request):
   """ A view to display all of the products available"""
 
   products = Product.objects.all()
-  print("products : ", products)
 
   context = {
         'products': products
     }
 
   return render(request, 'products/all_products.html', context)
+  
+
+def product_detail(request, product_id):
+    """ A view for an individual product information """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
