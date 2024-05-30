579f064bd6050647d1e11077afa0eeab7ec49f9d
Zoe Macf <zoemacfarlane93@gmail.com>
2024-05-30 11:40:36 +0000

    enable serving of static files

--- perfect_fit/settings.py
+++ perfect_fit/settings.py
@@ -29,7 +29,7 @@ if os.path.exists("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zoemacf-perfectfit-myjy0wrtjrs.ws-eu114.gitpod.io', '.herokuapp.com']
 
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
