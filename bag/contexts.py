781b15ceffa56b9e5d24f62fd09788dc8ca8a010
Zoe Macf <zoemacfarlane93@gmail.com>
2024-06-01 13:00:48 +0000

    fix delivery threshold not displaying

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,7 +22,7 @@ def bag_contents(request):
     'product_count': product_count,
     'delivery': delivery,
     'free_delivery_delta': free_delivery_delta,
-    'free delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+    'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
     'grand_total': grand_total,
   }
 
