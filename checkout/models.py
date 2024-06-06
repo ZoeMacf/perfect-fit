fd9ba585069a4be7cf0aa225e1bb0fa0151bdac3
Zoe Macf <zoemacfarlane93@gmail.com>
2024-06-06 10:20:18 +0000

    create checkout_success.html

--- checkout/models.py
+++ checkout/models.py
@@ -37,7 +37,7 @@ class Order(models.Model):
         accounting for delivery costs
         """
 
-        self.order_total = self.lineitems(Sum('order_item_total'))['order_item_total_sum']
+        self.order_total = self.lineitems(Sum('order_item_total'))['order_item_total_sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.bag_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else: 
