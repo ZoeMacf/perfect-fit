377f1d4303f1a6538d735534116ed550d0dfd401
Zoe Macf <zoemacfarlane93@gmail.com>
2024-06-10 10:33:10 +0000

    add edit_product view, template and url

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('', views.all_products, name='all_products'),
   path('<int:product_id>/', views.product_detail, name='product_detail'),
   path('add/', views.add_product, name='add_product'),
+  path('eidt/<int:product_id>/', views.edit_product, name='edit_product'),
 
 ]
