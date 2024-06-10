c09faed9a0853e86cd3440e8903dec67a21d875a
Zoe Macf <zoemacfarlane93@gmail.com>
2024-06-10 21:28:33 +0000

    update urls imports

--- perfect_fit/urls.py
+++ perfect_fit/urls.py
@@ -1,6 +1,8 @@
-from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
