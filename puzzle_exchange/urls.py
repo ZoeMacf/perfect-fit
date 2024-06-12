c1575b5898ec6824149d39282bc725284088b022
Zoe Theresa MacFarlane <zoemacfarlane93@gmail.com>
2024-06-12 13:52:19 +0000

    add templates for user_submission and puzzle_detail

--- puzzle_exchange/urls.py
+++ puzzle_exchange/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.view_puzzle_exchange, name='puzzle_exchange'),
+  path('puzzle_details/', views.puzzle_detail, name='puzzle_details'),
+  path('user_submissions/', views.user_submissions, name='user_submissions')
 ]
--- puzzle_exchange/views.py
+++ puzzle_exchange/views.py
@@ -6,3 +6,13 @@ def view_puzzle_exchange(request):
     """ view to render the puzzle exchange page """
 
     return render(request, 'puzzle_exchange/puzzle_exchange.html')
+
+def puzzle_detail(request):
+    """ view to render a detailed post for puzzle submission """
+
+    return render(request, 'puzzle_exchange/puzzle_details.html')
+
+def user_submissions(request):
+    """ view to render a list of users submissions """
+
+    return render(request, 'puzzle_exchange/user_submissions.html')
