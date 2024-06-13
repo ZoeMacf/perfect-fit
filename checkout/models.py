18c89ee4e021149dff65e714e4abbd3174bd8545
Zoe Theresa MacFarlane <zoemacfarlane93@gmail.com>
2024-06-13 20:26:55 +0000

    fix border and styling order_detail, checkout_success

--- checkout/models.py
+++ checkout/models.py
@@ -31,28 +31,29 @@ class Order(models.Model):
 
 
     def _generate_order_number(self):
-        """ generate a unique order number using UUID """
-
+        """
+        Generate a random, unique order number using UUID
+        """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        """ update the grand total each time a new item is added, 
-        accounting for delivery costs
         """
-
-        self.order_total = self.lineitems(Sum('order_item_total'))['order_item_total_sum'] or 0
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.bag_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-        else: 
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the order number
-        it if has not been set already
         """
-
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
