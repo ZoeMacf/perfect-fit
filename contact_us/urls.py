79e8e63b8a36f600ec6c2f8ba1c36567c297c150
Zoe Theresa MacFarlane <zoemacfarlane93@gmail.com>
2024-06-21 14:24:35 +0000

    fix contact us form

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.contact_us, name="contact_us"),
+    path("", views.contact_us_query, name="contact_us_query"),
     path("contact_us_success/", views.contact_success, name="contact_success"),
 ]
--- contact_us/views.py
+++ contact_us/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .forms import ContactUsForm
 from .models import ContactUs
 
 
-def contact_us(request):
+def contact_us_query(request):
     """view to create and send a contact query"""
 
     if request.method == "POST":
         form = ContactUsForm(request.POST)
         if form.is_valid():
+            form.save()
+
+        if form.is_valid():
             query = form.save()
-            confirm_query(request)
-            return redirct(reverse("contact_success"))
+            confirm_query(query)
+            return redirect(reverse("contact_success"))
         else:
             messages.error(
                 request, "Failed to send query, '\
@@ -31,13 +36,13 @@ def contact_us(request):
     return render(request, template, context)
 
 
-def confirm_query(request):
+def confirm_query(contact_us):
     """confirm query has been submitted"""
 
-    cust_email = request.user
+    cust_email = contact_us.email
     subject = render_to_string(
         "contact_us/contact_us_emails/contact_us_subject.txt",
-        {"msg_subject": msg_subject},
+        {"msg_subject": contact_us.msg_subject},
     )
     body = render_to_string(
         "contact_us/contact_us_emails/contact_us_body.txt",
@@ -51,4 +56,4 @@ def contact_success(request):
 
     template = "contact_us/contact_us_success.html"
 
-    return (render, template)
+    return render(request, template)
