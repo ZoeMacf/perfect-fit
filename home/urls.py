d86b3f2456e36b687edd8cc933c4356e3b5cc854
Zoe Theresa MacFarlane <zoemacfarlane93@gmail.com>
2024-06-22 21:11:10 +0000

    add privacy policy page

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="home")]
+urlpatterns = [
+    path("", views.index, name="home"),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+]
